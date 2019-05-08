e7ab2bac911ea84f20b88042a9d95abc43b19c33
mijipark1121 <mijipark1121@gmail.com>
2019-05-08 17:51:25 +0900

    minus 함수 추가

--- Caculator.java
+++ Caculator.java
@@ -10,5 +10,15 @@ public class Calculator {
 
   private static int sum(int a, int b) {
     retrurn a +b;
+
+  public static void main(String[] args) {
+      int a = 4;
+      int b = 2;
+      system.out.println( minus(a,b) );
+    }
+
+    private static int minus(int a, int b) {
+      retrurn a -b;  
+
   }
 }
