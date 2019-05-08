b6290736fb27b3e77595e61e009ab41df28c1741
mijipark1121 <mijipark1121@gmail.com>
2019-05-08 17:38:17 +0900

    sum 함수 추가

--- Caculator.java
+++ Caculator.java
@@ -3,5 +3,12 @@ package com.mycompany.test;
 public class Calculator {
 
   public static void main(String[] args) {
+    int a = 4;
+    int b = 2;
+    system.out.println( sum(a,b) );
+  }
+
+  private static int sum(int a, int b) {
+    retrurn a +b;
   }
 }
